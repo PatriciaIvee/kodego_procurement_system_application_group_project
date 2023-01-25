3d2e5795c5696ba7ab9f930346248863d12349f3
PatriciaIvee <patriciaiveeleones@gmail.com>
2023-01-25 09:29:49 +0800

    UPDATE 01-25-2023 Added TODO Note Nav_Graph activity_main.xml Added NavDrawerActivity.kt Removed ui.HomeActivity.kt
    Error
    7 issues were found when checking AAR metadata regarding Dependency
    Recommended action: Update this project to use a newer compileSdkVersion
          of at least 33, for example 33.
    targetSd and compileSdk changed from 32 to 33
    line 8 and 13 build.gradle
    
    Moved Intent-filter from .MainActivity to .NavDrawerActivity AndroidManifest.xml
    
    Added WindowsSoftInputMode on both Activities AndroidManifest.xml

---
+++ app/src/main/java/ph/kodego/victor/yu/leones/patricia/ivee/procurementsystemapplication/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package ph.kodego.victor.yu.leones.patricia.ivee.procurementsystemapplication.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
